6a5cc6f205bf336004ad014cede2cc3ef34fd1b1
ElnurKasimov <ekasimov2012@gmail.com>
2022-12-22 22:52:55 +0200

    init project

---
+++ src/main/java/group_project/MyNotebook/MyNotebookApplication.java
@@ -0,0 +1,13 @@
+package group_project.MyNotebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyNotebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyNotebookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/group_project/MyNotebook/MyNotebookApplicationTests.java
@@ -0,0 +1,13 @@
+package group_project.MyNotebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyNotebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
