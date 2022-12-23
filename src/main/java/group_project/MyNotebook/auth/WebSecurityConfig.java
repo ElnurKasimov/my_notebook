15fb541aa501eb2a276a97adc993bfa872fd207f
Maksym-Ivashchenko <76703094+Maksym-Ivashchenko@users.noreply.github.com>
2022-12-23 19:32:03 +0200

    Create WebSecurityConfig.java

---
+++ src/main/java/group_project/MyNotebook/auth/WebSecurityConfig.java
@@ -0,0 +1,53 @@
+package group_project.MyNotebook.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true,
+        securedEnabled = true,
+        jsr250Enabled = true)
+public class WebSecurityConfig {
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/", "/register", "/login", "/css/**", "/js/**", "/img/**", "/resources/**")
+                .permitAll()
+                .requestMatchers("/admin/**").hasAuthority("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll().deleteCookies()
+        ;
+        return http.build();
+    }
+
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        return new MyUserJdbcDetailsService(jdbcTemplate);
+//    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
