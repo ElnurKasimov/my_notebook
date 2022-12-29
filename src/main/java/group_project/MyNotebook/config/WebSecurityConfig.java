7210a817d3249989ce47e3bf30c474b7b1803da6
Elnur <elnur.kasimov@grand-logistic.com>
2022-12-29 15:40:36 +0200

    resolved impossibility to use H2

--- src/main/java/group_project/MyNotebook/config/WebSecurityConfig.java
+++ src/main/java/group_project/MyNotebook/config/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -27,6 +28,7 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
                 .authorizeHttpRequests()
                 .requestMatchers("/", "/register/**", "/login", "/css/**", "/js/**", "/img/**", "/resources/**")
                 .permitAll()
